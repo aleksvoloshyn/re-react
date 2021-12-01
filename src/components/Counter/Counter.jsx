d1e6e5c1afed0450aa57117bfe8df0474c8529d7
Alexei Voloshyn <75684845+aleksvoloshyn@users.noreply.github.com>
2021-12-01 22:36:28 +0200

    add

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import s from '../../styles/Button.module.css'
 
 class Counter extends Component {
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     value: 0,
+  //     step: 15,
+  //   }
+  // }
   state = {
     value: 0,
-    step: 11,
+    step: 15,
+    visible: true,
   }
-
-  /* ... */
 
   increase = () => {
     this.setState((prevState) => {
@@ -21,12 +27,35 @@ class Counter extends Component {
     })
   }
 
+  nullValue = () => {
+    this.setState({ value: 0 })
+  }
+
+  setVisible = () => {
+    if (this.state.visible) {
+      this.setState({ visible: false })
+      console.log(this.state.visible)
+    } else {
+      this.setState({ visible: true })
+      console.log(this.state.visible)
+    }
+  }
+
   render() {
     return (
       <div>
         <span>{this.state.value}</span>
-        <button onClick={this.increase}>+</button>
-        <button onClick={this.decrease}>-</button>
+        <button className={s.Button} onClick={this.increase}>
+          + {this.state.step}
+        </button>
+        <button className={s.Button} onClick={this.nullValue}>
+          0
+        </button>
+        <button className={s.Button} onClick={this.decrease}>
+          - {this.state.step}
+        </button>
+        <button onClick={this.setVisible}>DISAPEAR</button>
+        {this.state.visible && <span>ИСЧЕЗАЙКА</span>}
       </div>
     )
   }
